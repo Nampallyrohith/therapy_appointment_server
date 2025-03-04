88f7d5c3c31e3b6d5693bed67845659a28ab629f
Nampally Rohith <rohithnampelly57@gmail.com>
2025-03-04 19:11:15 +0530

    nested the routes in route if same

--- src/handlers/routes.ts
+++ src/handlers/routes.ts
@@ -43,131 +43,133 @@ router.post(
   })
 );
 
-router.get(
-  "/user/profile-info/:googleUserId",
-  defineRoute(async (req, res) => {
-    try {
-      const { googleUserId } = req.params;
-      const user = await getUserByGoogleId(googleUserId);
-      return res.status(200).send({
-        message: "User Successfully retrived.",
-        userDetails: user.userInfo,
-        userMeta: user.userMeta,
-      });
-    } catch (error) {
-      if (error instanceof NotFound) {
-        return res.status(400).json("User doesn't exists.");
-      }
-      return res.status(500).json({ error: error });
-    }
-  })
-);
-
 router.use(
-  "/user/appointment",
+  "/user",
   router.get(
-    "/therapies",
-    defineRoute(async (req, res) => {
-      const response = await getAllTherapies();
-      res.status(200).send({
-        message: "Successfully therapies retrived.",
-        therapies: response,
-      });
-    })
-  ),
-
-  router.get(
-    "/:therapyId/doctors",
-    defineRoute(async (req, res) => {
-      const { therapyId } = req.params;
-      const response = await getAllDoctorsByTherapyId(therapyId);
-      res.status(200).send({
-        message: "Successfully doctor's retrived.",
-        doctors: response,
-      });
-    })
-  ),
-
-  router.get(
-    "/:doctorId/available_date",
-    defineRoute(async (req, res) => {
-      const { doctorId } = req.params;
-
-      const response = await getAvailableDates(doctorId);
-      res.status(200).send({
-        message: "Successfully retrieved doctor's date and time",
-        date: response,
-      });
-    })
-  ),
-
-  router.get(
-    "/:doctorId/available_time",
-    defineRoute(async (req, res) => {
-      const { doctorId } = req.params;
-      const date = Array.isArray(req.query.date)
-        ? req.query.date[0]
-        : req.query.date;
-
-      if (!date || typeof date !== "string") {
-        return res
-          .status(400)
-          .send({ message: "Invalid or missing date parameter." });
-      }
-
-      const response = await getAvailableDates(doctorId, date);
-      res.status(200).send({
-        message: "Successfully retrieved doctor's time",
-        time: response,
-      });
-    })
-  ),
-  router.post(
-    "/create-event/:googleUserId",
+    "/profile-info/:googleUserId",
     defineRoute(async (req, res) => {
       try {
         const { googleUserId } = req.params;
-        const event = eventSchema.parse(req.body);
-        await insertEventInfo(googleUserId, event);
-        res
-          .status(201)
-          .send({ ok: true, message: "Event inserted successfully" });
+        const user = await getUserByGoogleId(googleUserId);
+        return res.status(200).send({
+          message: "User Successfully retrived.",
+          userDetails: user.userInfo,
+          userMeta: user.userMeta,
+        });
       } catch (error) {
         if (error instanceof NotFound) {
-          return res.status(400).json({ error: "User doesn't exists." });
+          return res.status(400).json("User doesn't exists.");
         }
         return res.status(500).json({ error: error });
+      }
+    })
+  ),
+
+  router.use(
+    "/appointment",
+    router.get(
+      "/therapies",
+      defineRoute(async (req, res) => {
+        const response = await getAllTherapies();
+        res.status(200).send({
+          message: "Successfully therapies retrived.",
+          therapies: response,
+        });
+      })
+    ),
+
+    router.get(
+      "/:therapyId/doctors",
+      defineRoute(async (req, res) => {
+        const { therapyId } = req.params;
+        const response = await getAllDoctorsByTherapyId(therapyId);
+        res.status(200).send({
+          message: "Successfully doctor's retrived.",
+          doctors: response,
+        });
+      })
+    ),
+
+    router.get(
+      "/:doctorId/available_date",
+      defineRoute(async (req, res) => {
+        const { doctorId } = req.params;
+
+        const response = await getAvailableDates(doctorId);
+        res.status(200).send({
+          message: "Successfully retrieved doctor's date and time",
+          date: response,
+        });
+      })
+    ),
+
+    router.get(
+      "/:doctorId/available_time",
+      defineRoute(async (req, res) => {
+        const { doctorId } = req.params;
+        const date = Array.isArray(req.query.date)
+          ? req.query.date[0]
+          : req.query.date;
+
+        if (!date || typeof date !== "string") {
+          return res
+            .status(400)
+            .send({ message: "Invalid or missing date parameter." });
+        }
+
+        const response = await getAvailableDates(doctorId, date);
+        res.status(200).send({
+          message: "Successfully retrieved doctor's time",
+          time: response,
+        });
+      })
+    ),
+    router.post(
+      "/create-event/:googleUserId",
+      defineRoute(async (req, res) => {
+        try {
+          const { googleUserId } = req.params;
+          const event = eventSchema.parse(req.body);
+          await insertEventInfo(googleUserId, event);
+          res
+            .status(201)
+            .send({ ok: true, message: "Event inserted successfully" });
+        } catch (error) {
+          if (error instanceof NotFound) {
+            return res.status(400).json({ error: "User doesn't exists." });
+          }
+          return res.status(500).json({ error: error });
+        }
+      })
+    )
+  ),
+  router.get(
+    "/my-appointments/:googleUserId",
+    defineRoute(async (req, res) => {
+      try {
+        const { googleUserId } = req.params;
+        const response = await getAllAppointments(googleUserId);
+
+        res.status(200).send({
+          message: "Successfully retrieved appointments",
+          appointments: response,
+        });
+      } catch (err) {
+        if (err instanceof NotFound) {
+          res.status(400).send({
+            error: "User doesn't exist",
+          });
+        }
+        res.status(500).send({
+          error: err,
+        });
       }
     })
   )
 );
 
 router.get(
-  "/user/my-appointments/:googleUserId",
-  defineRoute(async (req, res) => {
-    try {
-      const { googleUserId } = req.params;
-      const response = await getAllAppointments(googleUserId);
-
-      res.status(200).send({
-        message: "Successfully retrieved appointments",
-        appointments: response,
-      });
-    } catch (err) {
-      if (err instanceof NotFound) {
-        res.status(400).send({
-          error: "User doesn't exist",
-        });
-      }
-      res.status(500).send({
-        error: err,
-      });
-    }
-  })
-);
-
-router.get(
-  "/user/doctors",
+  "/doctors",
   defineRoute(async (req, res) => {
     const response = await getAllDoctors();
     res.status(200).send({
@@ -175,18 +177,19 @@ router.get(
       doctors: response,
     });
   })
-),
-  router.get(
-    "/doctor/:doctorId",
-    defineRoute(async (req, res) => {
-      const { doctorId } = req.params;
-      const response = await getDoctorById(Number(doctorId));
-      res.status(200).send({
-        message: "Doctor successfully retrieved based on doctorId",
-        doctor: response,
-      });
-    })
-  );
+);
+
+router.get(
+  "/doctor/:doctorId",
+  defineRoute(async (req, res) => {
+    const { doctorId } = req.params;
+    const response = await getDoctorById(Number(doctorId));
+    res.status(200).send({
+      message: "Doctor successfully retrieved based on doctorId",
+      doctor: response,
+    });
+  })
+);
 
 // TODO: Complete profile details of doctor
 router.post(
